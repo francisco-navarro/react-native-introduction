5138be4dfbd64106de3ffcac89ab5f26dd03cf02
Paco Navarro <packarbell@gmail.com>
2017-11-20 20:30:06 +0100

    inmutability

--- app/config/store.js
+++ app/config/store.js
@@ -1,9 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
-// import reducers from '../reducers';
+import reducers from '../reducers/index';
 import { createLogger } from 'redux-logger';
+import immutableCheckMiddleWare from 'redux-immutable-state-invariant';
 
 // Setup
 const middleWare = [];
+
+// Immutability Check
+if (process.env.NODE_ENV === 'development') {
+  middleWare.push(immutableCheckMiddleWare());
+}
 
 // Logger Middleware. This always has to be last
 const loggerMiddleware = createLogger({
